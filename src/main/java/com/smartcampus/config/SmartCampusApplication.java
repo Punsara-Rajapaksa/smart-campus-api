37bb02bff92472b8db8b36418966876e216bd899
Punsara Rajapaksa <punsararajapaksa@gmail.com>
2026-04-15 09:45:58 +0530

    Part 1: Project setup and Discovery endpoint

---
+++ src/main/java/com/smartcampus/config/SmartCampusApplication.java
@@ -0,0 +1,18 @@
+package com.smartcampus.config;
+
+import com.smartcampus.resource.DiscoveryResource;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/api/v1")
+public class SmartCampusApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(DiscoveryResource.class);
+        return classes;
+    }
+}
